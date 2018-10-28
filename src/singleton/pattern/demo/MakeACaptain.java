cd69115f798c84efa40e0d9eb492aa19ce9af933
Ashutosh Ranjan <ashutosh.ranjan@comviva.com>
2018-10-28 11:58:33 +0530

    singleton pattern commit

--- src/singleton/pattern/demo/MakeACaptain.java
+++ src/singleton/pattern/demo/MakeACaptain.java
@@ -1,5 +1,25 @@
 package singleton.pattern.demo;
 
 public class MakeACaptain {
+private static MakeACaptain _captain;
+
+	private MakeACaptain() {
+	}
+	
+public static MakeACaptain getCaptain() {
+	//Lazy initialization
+			if(_captain == null) {
+				_captain = new MakeACaptain();
+				System.out.println("New Captain selected for our team");
+			}
+			else
+			{
+				  System.out.print("You already have a Captain for your team."); 
+				  System.out.println("Send him for the toss.");
+			}
+			return _captain;
+	
+}
+
 
 }
